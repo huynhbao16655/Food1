2a7ed6833d7801f2f583a1038e57a78010f31e91
Bảo <huynhquocbaolx2001@gmail.com>
2023-03-10 21:57:08 +0700

    Initial commit

---
+++ app/src/main/java/baotoan/food/activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package baotoan.food.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import baotoan.food.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/baotoan/food/activities/SplashActivity.java
@@ -0,0 +1,29 @@
+package baotoan.food.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import baotoan.food.R;
+import baotoan.food.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+        binding.tvgetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
