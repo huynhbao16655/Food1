891622e04266dae85a9828bc645ad7ce1cf48ff7
Bảo <huynhquocbaolx2001@gmail.com>
2023-03-11 18:36:39 +0700

    update

--- app/src/main/java/baotoan/food/activities/HomeActivity.java
+++ app/src/main/java/baotoan/food/activities/HomeActivity.java
@@ -1,29 +1,43 @@
 package baotoan.food.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import baotoan.food.R;
+import baotoan.food.adapters.CategoryAdapter;
+import baotoan.food.databinding.ActivityHomeBinding;
 import baotoan.food.viewModel.HomeViewModel;
 
 public class HomeActivity extends AppCompatActivity {
     HomeViewModel homeViewModel;
+    ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home);
+        intiVew();
         intiData();
+    }
+
+    private void intiVew(){
+        binding.rcCategory.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        binding.rcCategory.setLayoutManager(layoutManager);
     }
 
     private void intiData(){
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         homeViewModel.categoryModelMutableLiveData().observe(this, categoryModel -> {
             if( categoryModel.isSuccess()){
-                Log.d("logg", categoryModel.getResult().get(0).getCategory());
+                CategoryAdapter adapter = new CategoryAdapter(categoryModel.getResult());
+                binding.rcCategory.setAdapter(adapter);
             }
         });
     }
---
+++ app/src/main/java/baotoan/food/adapters/CategoryAdapter.java
@@ -0,0 +1,54 @@
+package baotoan.food.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import baotoan.food.databinding.ItemCategoryBinding;
+import baotoan.food.model.Category;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyViewHolder> {
+    List<Category> list;
+
+    public CategoryAdapter(List<Category> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemCategoryBinding itemCategoryBinding = ItemCategoryBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new MyViewHolder(itemCategoryBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.setBinding(list.get(position));
+        Glide.with(holder.itemView).load(list.get(position).getCategoryThumb()).into(holder.binding.imageCate);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        private ItemCategoryBinding binding;
+
+        public MyViewHolder(ItemCategoryBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void setBinding(Category category){
+            binding.setCategory(category);
+        }
+    }
+}
